6bd137417dea17d7755c94fd6202256f970e31e1
Giovanni Cascione <ing.cascione@gmail.com>
2024-11-21 23:53:07 +0100

    add generated files

---
+++ src/dep/generated/build_details.h
@@ -0,0 +1,8 @@
+#define BUILD_TIME 12:25:30
+#define BUILD_DATE Nov 19 2024
+#define BUILD_CHAR ANSI
+#define BUILD_CPU  X86
+#define BUILD_COMP Visual C++ 2010
+#ifndef _MSC_VER
+ #define _MSC_VER  1600
+#endif
